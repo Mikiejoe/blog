62030db675ac8e502d8f18371190dc857297f230
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-07-19 14:03:55 +0300

    Add: effect on the active link

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { HiMenuAlt1, HiMenuAlt2, HiMenuAlt3, HiMenuAlt4 } from "react-icons/hi";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Links = [
   {
@@ -28,6 +29,12 @@ function NavBar() {
     setIsOpen(!isOpen);
     console.log("navbar open", isOpen);
   };
+  const CurrentRoute = (current) => {
+    const location = useLocation();
+    console.log(current === location.pathname);
+    return current === location.pathname;
+  };
+  // console.log(CurrentRoute());
   return (
     <>
       <div className="bg-primaryText flex p-4 md:px-16 justify-between items-center text-primary">
@@ -36,7 +43,9 @@ function NavBar() {
           {Links.map((link, index) => (
             <Link
               key={index}
-              className=" border-b-[2px] border-primaryText px-1 py-1 hover:border-primary transition-colors"
+              className={` border-b-[2px] border-primaryText px-1 py-1 hover:border-primary transition-colors ${
+                CurrentRoute(link.route) ? "border-primary" : ""
+              }`}
               to={link.route}
             >
               <li>{link.name}</li>
@@ -62,10 +71,18 @@ function NavBar() {
             <Link
               onClick={openNavBar}
               key={index}
-              className="border-b-[2px] border-primaryText px-1 py-1 hover:border-primary transition-colors flex"
+              className={`border-b-[2px] border-primaryText px-1 py-1 ${
+                CurrentRoute(link.route) ? "text-primary" : ""
+              } hover:bg-gray-700 rounded-md w-[40%] transition-colors flex`}
               to={link.route}
             >
-              <li className="list-none text-links">{link.name}</li>
+              <li
+                className={`list-none text-links ${
+                  CurrentRoute(link.route) ? "text-primary" : ""
+                }`}
+              >
+                {link.name}
+              </li>
             </Link>
           ))}
         </div>

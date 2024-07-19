7d01d6e7ff0e172dfd86c9390f14628cf74e3c85
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-07-19 13:33:20 +0300

    add: Lazyloading effect

--- src/components/homepage/About.jsx
+++ src/components/homepage/About.jsx
@@ -1,23 +1,48 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 function About() {
   return (
     <>
-    <div className='space-y-4'>
-      <h1 className='uppercase text-primaryText/70 text-lg font-bold'>About Us</h1>
-      <div className='md:flex space-y-2 items-center gap-52'>
-      <p className='text-primaryText/60'>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate perspiciatis dolorum molestiae aperiam temporibus ut corrupti doloremque beatae. Quae, impedit. At commodi distinctio temporibus fuga natus ipsum labore id maiores odio voluptatum, harum quam inventore minima amet cum! Quasi fuga delectus ducimus, eaque nihil aliquam tempora nisi repudiandae nemo earum facere quae praesentium laboriosam, ullam laudantium repellat ipsum ipsa voluptatum.
-      </p>
-      <img src="https://media.istockphoto.com/id/472324721/vector/rally-demonstration.jpg?s=1024x1024&w=is&k=20&c=ZEhCQQSiV_v5WvWJsTztpXmwxPAWud4CJCtpETdxq70=" alt="" className='h-[240px] hidden md:block bg-gray-400' />
+      <div className="space-y-4">
+        <h1 className="uppercase text-primaryText/70 text-lg font-bold">
+          About Us
+        </h1>
+        <div className="md:flex space-y-2 items-center gap-52">
+          <p className="text-primaryText/60">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate
+            perspiciatis dolorum molestiae aperiam temporibus ut corrupti
+            doloremque beatae. Quae, impedit. At commodi distinctio temporibus
+            fuga natus ipsum labore id maiores odio voluptatum, harum quam
+            inventore minima amet cum! Quasi fuga delectus ducimus, eaque nihil
+            aliquam tempora nisi repudiandae nemo earum facere quae praesentium
+            laboriosam, ullam laudantium repellat ipsum ipsa voluptatum.
+          </p>
+          <LazyLoadImage
+            className="bg-red-600 h-[250px] w-[2600px] "
+            alt="altanate"
+            effect="blur"
+            height="240px"
+            placeholder={<div className='h-[240px] bg-gray-400'></div>}
+            wrapperProps={{
+              // If you need to, you can tweak the effect transition using the wrapper style.
+              style: { transitionDelay: "3s" },
+            }}
+            src="https://media.istockphoto.com/id/472324721/vector/rally-demonstration.jpg?s=1024x1024&w=is&k=20&c=ZEhCQQSiV_v5WvWJsTztpXmwxPAWud4CJCtpETdxq70="
+          />
+
+          {/* <img src="https://media.istockphoto.com/id/472324721/vector/rally-demonstration.jpg?s=1024x1024&w=is&k=20&c=ZEhCQQSiV_v5WvWJsTztpXmwxPAWud4CJCtpETdxq70=" alt="" className='h-[240px] hidden md:block bg-gray-400' /> */}
+        </div>
+        <Link to="/about">
+          <button className="bg-primary border-primaryText border rounded-md text-primaryText/50 p-2 mt-4">
+            Learn More
+          </button>
+        </Link>
       </div>
-      <Link to="/about">
-        <button className='bg-primary border-primaryText border rounded-md text-primaryText/50 p-2 mt-4'>Learn More</button>
-      </Link>
-    </div>
     </>
-  )
+  );
 }
 
-export default About
+export default About;
